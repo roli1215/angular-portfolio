abc7c718dca7c9fb1efbdbd19414f24e3f4498e5
ROLI\user <karczubroland@gmail.com>
2026-03-09 20:41:33 +0100

    Új design alakítás elkezdése

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'about',
@@ -7,20 +7,6 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements AfterViewInit {
-  imageVisible = false;
-  nameVisible = false;
-
-  ngAfterViewInit(): void {
-    // Kép animáció rögtön betöltéskor
-    setTimeout(() => {
-      this.imageVisible = true;
-    }, 100); // kis delay, hogy a betöltés után induljon
-
-    // Név animáció, rövid késéssel
-    setTimeout(() => {
-      this.nameVisible = true;
-    }, 800); // 0.8s késés
-  }
+export class AboutComponent {
 
 }
